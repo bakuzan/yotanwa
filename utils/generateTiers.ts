f42ebef6a0dd97ae9fe5ac4230c050062a7fc8e1
bakuzan <imannoyed4days@hotmail.co.uk>
2020-01-22 12:38:45 +0000

    Formatting.

--- utils/generateTiers.ts
+++ utils/generateTiers.ts
@@ -7,16 +7,10 @@ export default function generateTiers(
 
   const tiers = t
     .filter(([s]) => !excludedScores.has(s))
-    .reduce(
-      (m, [score, tier]) => {
-        const curr = m.get(tier) || [];
-        return m.set(tier, [
-          ...curr,
-          ...items.filter((x) => x.score === score)
-        ]);
-      },
-      new Map() as Map<string, any[]>
-    );
+    .reduce((m, [score, tier]) => {
+      const curr = m.get(tier) || [];
+      return m.set(tier, [...curr, ...items.filter((x) => x.score === score)]);
+    }, new Map() as Map<string, any[]>);
 
   return Array.from(tiers.entries());
 }
